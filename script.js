eb04e4c523373f3bd59ab4e762014a41743ad8e5
Md. Arshad <arshadchowdhury46@gmail.com>
2023-11-12 21:32:32 +0600

    Added to do list add functionality and designed a bit

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementsByClassName("input-box");
+const form = document.getElementsByClassName("form-container");
+const toDoItem = document.getElementsByClassName("to-do-list");
+
+form[0].onsubmit = function trying(event) {
+  event.preventDefault();
+  const toDoName = input[0].value;
+  const singleToDo = document.createElement("ul");
+  const listToDo = document.createElement("li");
+  toDoItem[0].append(singleToDo);
+  singleToDo.append(listToDo);
+  listToDo.append(toDoName);
+  input[0].value = "";
+};
